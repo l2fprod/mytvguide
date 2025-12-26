534191ed1cb5a508d465b47c38a74ef7453e2694
Frederic Lavigne <fred@L2FProd.com>
2025-12-25 19:23:11 -0600

    fix production build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/mytvguide/' : '/',
   root: '.',
   // Serve files from `public` as static assets and keep project root
   // as Vite root so `index.html` can import `/src/*` modules.
